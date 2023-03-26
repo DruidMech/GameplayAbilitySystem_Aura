e84365557a09d69e619376985444b259c386d991
Stephen Ulibarri <druidmech@gmail.com>
2023-03-26 11:13:56 -0600

    Enhanced Input

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright Druid Mechanics
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
