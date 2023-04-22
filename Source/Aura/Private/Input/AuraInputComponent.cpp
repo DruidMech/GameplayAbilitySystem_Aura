e5dfcc9dbf0dea99ebb4f43e8f50628c424e0c7f
Stephen Ulibarri <druidmech@gmail.com>
2023-04-21 19:32:31 -0600

    Aura Input Component

---
+++ Source/Aura/Private/Input/AuraInputComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Druid Mechanics
+
+
+#include "Input/AuraInputComponent.h"
+
---
+++ Source/Aura/Public/Input/AuraInputComponent.h
@@ -0,0 +1,47 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "AuraInputConfig.h"
+#include "AuraInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+public:
+	template<class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+	void BindAbilityActions(const UAuraInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc);
+};
+
+template <class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+void UAuraInputComponent::BindAbilityActions(const UAuraInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc)
+{
+	check(InputConfig);
+
+	for (const FAuraInputAction& Action : InputConfig->AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag.IsValid())
+		{
+			if (PressedFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Started, Object, PressedFunc, Action.InputTag);
+			}
+
+			if (ReleasedFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Completed, Object, ReleasedFunc, Action.InputTag);
+			}
+			
+			if (HeldFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Triggered, Object, HeldFunc, Action.InputTag);
+			}
+		}
+	}
+}
