ac9de372ec24598d2eed003f4fe177b7684dcd06
Stephen Ulibarri <druidmech@gmail.com>
2023-04-23 16:52:40 -0600

    Aura Projectile Spell

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,15 @@
+// Copyright Druid Mechanics
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("ActivateAbility (C++)"), true, true, FLinearColor::Yellow, 3);
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,20 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
