719bfd3cd0ac56b78c772fea0ce0bbd113e60452
Stephen Ulibarri <druidmech@gmail.com>
2023-05-01 18:08:15 -0600

    Floating Text Widget

---
+++ Source/Aura/Private/UI/Widget/DamageTextComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Druid Mechanics
+
+
+#include "UI/Widget/DamageTextComponent.h"
+
---
+++ Source/Aura/Public/UI/Widget/DamageTextComponent.h
@@ -0,0 +1,19 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "DamageTextComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UDamageTextComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void SetDamageText(float Damage);
+};
