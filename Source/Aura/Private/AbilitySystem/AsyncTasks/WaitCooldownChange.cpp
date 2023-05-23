3b94ce5c1d61ccc1458a7ff16699dfe5dbd1bca6
Stephen Ulibarri <druidmech@gmail.com>
2023-05-22 20:10:42 -0600

    Cooldown Async Task

---
+++ Source/Aura/Private/AbilitySystem/AsyncTasks/WaitCooldownChange.cpp
@@ -0,0 +1,75 @@
+// Copyright Druid Mechanics
+
+
+#include "AbilitySystem/AsyncTasks/WaitCooldownChange.h"
+#include "AbilitySystemComponent.h"
+
+UWaitCooldownChange* UWaitCooldownChange::WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& InCooldownTag)
+{
+	UWaitCooldownChange* WaitCooldownChange = NewObject<UWaitCooldownChange>();
+	WaitCooldownChange->ASC = AbilitySystemComponent;
+	WaitCooldownChange->CooldownTag = InCooldownTag;
+	
+	if (!IsValid(AbilitySystemComponent) || !InCooldownTag.IsValid())
+	{
+		WaitCooldownChange->EndTask();
+		return nullptr;
+	}
+
+	// To know when a cooldown has ended (Cooldown Tag has been removed)
+	AbilitySystemComponent->RegisterGameplayTagEvent(
+		InCooldownTag,
+		EGameplayTagEventType::NewOrRemoved).AddUObject(
+			WaitCooldownChange,
+			&UWaitCooldownChange::CooldownTagChanged);
+
+	// To know when a cooldown effect has been applied
+	AbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(WaitCooldownChange, &UWaitCooldownChange::OnActiveEffectAdded);
+
+	return WaitCooldownChange;
+}
+
+void UWaitCooldownChange::EndTask()
+{
+	if (!IsValid(ASC)) return;
+	ASC->RegisterGameplayTagEvent(CooldownTag, EGameplayTagEventType::NewOrRemoved).RemoveAll(this);
+
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UWaitCooldownChange::CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount)
+{
+	if (NewCount == 0)
+	{
+		CooldownEnd.Broadcast(0.f);
+	}
+}
+
+void UWaitCooldownChange::OnActiveEffectAdded(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	FGameplayTagContainer AssetTags;
+	SpecApplied.GetAllAssetTags(AssetTags);
+
+	FGameplayTagContainer GrantedTags;
+	SpecApplied.GetAllGrantedTags(GrantedTags);
+
+	if (AssetTags.HasTagExact(CooldownTag) || GrantedTags.HasTagExact(CooldownTag))
+	{
+		FGameplayEffectQuery GameplayEffectQuery = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(CooldownTag.GetSingleTagContainer());
+		TArray<float> TimesRemaining = ASC->GetActiveEffectsTimeRemaining(GameplayEffectQuery);
+		if (TimesRemaining.Num() > 0)
+		{
+			float TimeRemaining = TimesRemaining[0];
+			for (int32 i = 0; i < TimesRemaining.Num(); i++)
+			{
+				if (TimesRemaining[i] > TimeRemaining)
+				{
+					TimeRemaining = TimesRemaining[i];
+				}
+			}
+			
+			CooldownStart.Broadcast(TimeRemaining);
+		}
+	}
+}
---
+++ Source/Aura/Public/AbilitySystem/AsyncTasks/WaitCooldownChange.h
@@ -0,0 +1,43 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GameplayTagContainer.h"
+#include "ActiveGameplayEffectHandle.h"
+#include "WaitCooldownChange.generated.h"
+
+class UAbilitySystemComponent;
+struct FGameplayEffectSpec;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCooldownChangeSignature, float, TimeRemaining);
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UWaitCooldownChange : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature CooldownStart;
+
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature CooldownEnd;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
+	static UWaitCooldownChange* WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& InCooldownTag);
+
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+protected:
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> ASC;
+
+	FGameplayTag CooldownTag;
+
+	void CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount);
+	void OnActiveEffectAdded(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle);
+};
