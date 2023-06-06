f351d7d3d2e7ddefbf6647cf2cde73d6ba93d3d8
Stephen Ulibarri <druidmech@gmail.com>
2023-06-05 18:00:09 -0600

    Point Collection

---
+++ Source/Aura/Private/Actor/PointCollection.cpp
@@ -0,0 +1,103 @@
+// Copyright Druid Mechanics
+
+
+#include "Actor/PointCollection.h"
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+APointCollection::APointCollection()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Pt_0 = CreateDefaultSubobject<USceneComponent>("Pt_0");
+	ImmutablePts.Add(Pt_0);
+	SetRootComponent(Pt_0);
+
+	Pt_1 = CreateDefaultSubobject<USceneComponent>("Pt_1");
+	ImmutablePts.Add(Pt_1);
+	Pt_1->SetupAttachment(GetRootComponent());
+
+	Pt_2 = CreateDefaultSubobject<USceneComponent>("Pt_2");
+	ImmutablePts.Add(Pt_2);
+	Pt_2->SetupAttachment(GetRootComponent());
+
+	Pt_3 = CreateDefaultSubobject<USceneComponent>("Pt_3");
+	ImmutablePts.Add(Pt_3);
+	Pt_3->SetupAttachment(GetRootComponent());
+	
+	Pt_4 = CreateDefaultSubobject<USceneComponent>("Pt_4");
+	ImmutablePts.Add(Pt_4);
+	Pt_4->SetupAttachment(GetRootComponent());
+
+	Pt_5 = CreateDefaultSubobject<USceneComponent>("Pt_5");
+	ImmutablePts.Add(Pt_5);
+	Pt_5->SetupAttachment(GetRootComponent());
+	
+	Pt_6 = CreateDefaultSubobject<USceneComponent>("Pt_6");
+	ImmutablePts.Add(Pt_6);
+	Pt_6->SetupAttachment(GetRootComponent());
+
+	Pt_7 = CreateDefaultSubobject<USceneComponent>("Pt_7");
+	ImmutablePts.Add(Pt_7);
+	Pt_7->SetupAttachment(GetRootComponent());
+
+	Pt_8 = CreateDefaultSubobject<USceneComponent>("Pt_8");
+	ImmutablePts.Add(Pt_8);
+	Pt_8->SetupAttachment(GetRootComponent());
+
+	Pt_9 = CreateDefaultSubobject<USceneComponent>("Pt_9");
+	ImmutablePts.Add(Pt_9);
+	Pt_9->SetupAttachment(GetRootComponent());
+
+	Pt_10 = CreateDefaultSubobject<USceneComponent>("Pt_10");
+	ImmutablePts.Add(Pt_10);
+	Pt_10->SetupAttachment(GetRootComponent());
+
+	
+}
+
+TArray<USceneComponent*> APointCollection::GetGroundPoints(const FVector& GroundLocation, int32 NumPoints, float YawOverride)
+{
+	checkf(ImmutablePts.Num() >= NumPoints, TEXT("Attempted to access ImmutablePts out of bounds."));
+
+	TArray<USceneComponent*> ArrayCopy;
+
+	for (USceneComponent* Pt : ImmutablePts)
+	{
+		if (ArrayCopy.Num() >= NumPoints) return ArrayCopy;
+
+		if (Pt != Pt_0)
+		{
+			FVector ToPoint = Pt->GetComponentLocation() - Pt_0->GetComponentLocation();
+			ToPoint = ToPoint.RotateAngleAxis(YawOverride, FVector::UpVector);
+			Pt->SetWorldLocation(Pt_0->GetComponentLocation() + ToPoint);
+		}
+
+		const FVector RaisedLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, Pt->GetComponentLocation().Z + 500.f);
+		const FVector LoweredLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, Pt->GetComponentLocation().Z - 500.f);
+
+		FHitResult HitResult;
+		TArray<AActor*> IgnoreActors;
+		UAuraAbilitySystemLibrary::GetLivePlayersWithinRadius(this, IgnoreActors, TArray<AActor*>(), 1500.f, GetActorLocation());
+
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActors(IgnoreActors);
+		GetWorld()->LineTraceSingleByProfile(HitResult, RaisedLocation, LoweredLocation, FName("BlockAll"), QueryParams);
+
+		const FVector AdjustedLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, HitResult.ImpactPoint.Z);
+		Pt->SetWorldLocation(AdjustedLocation);
+		Pt->SetWorldRotation(UKismetMathLibrary::MakeRotFromZ(HitResult.ImpactNormal));
+
+		ArrayCopy.Add(Pt);
+	}
+	return ArrayCopy;
+}
+
+void APointCollection::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Public/Actor/PointCollection.h
@@ -0,0 +1,60 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PointCollection.generated.h"
+
+UCLASS()
+class AURA_API APointCollection : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	APointCollection();
+
+	UFUNCTION(BlueprintPure)
+	TArray<USceneComponent*> GetGroundPoints(const FVector& GroundLocation, int32 NumPoints, float YawOverride = 0.f);
+	
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TArray<USceneComponent*> ImmutablePts;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_0;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_1;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_2;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_3;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_4;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_5;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_6;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_7;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_8;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_9;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_10;
+
+	
+};
