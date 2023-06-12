df25439d710bda6fb5d116ddf1221fe02536460a
Stephen Ulibarri <druidmech@gmail.com>
2023-06-12 16:36:19 -0600

    Data Structures for Saving Data

--- Source/Aura/Private/Game/LoadScreenSaveGame.cpp
+++ Source/Aura/Private/Game/LoadScreenSaveGame.cpp
@@ -3,3 +3,26 @@
 
 #include "Game/LoadScreenSaveGame.h"
 
+FSavedMap ULoadScreenSaveGame::GetSavedMapWithMapName(const FString& InMapName)
+{
+	for (const FSavedMap& Map : SavedMaps)
+	{
+		if (Map.MapAssetName == InMapName)
+		{
+			return Map;
+		}
+	}
+	return FSavedMap();
+}
+
+bool ULoadScreenSaveGame::HasMap(const FString& InMapName)
+{
+	for (const FSavedMap& Map : SavedMaps)
+	{
+		if (Map.MapAssetName == InMapName)
+		{
+			return true;
+		}
+	}
+	return false;
+}
--- Source/Aura/Public/Game/LoadScreenSaveGame.h
+++ Source/Aura/Public/Game/LoadScreenSaveGame.h
@@ -16,6 +16,39 @@ enum ESaveSlotStatus
 	Taken
 };
 
+USTRUCT()
+struct FSavedActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FName ActorName = FName();
+
+	UPROPERTY()
+	FTransform Transform = FTransform();
+
+	// Serialized variables from the Actor - only those marked with SaveGame specifier
+	UPROPERTY()
+	TArray<uint8> Bytes;
+};
+
+inline bool operator==(const FSavedActor& Left, const FSavedActor& Right)
+{
+	return Left.ActorName == Right.ActorName;
+}
+
+USTRUCT()
+struct FSavedMap
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FString MapAssetName = FString();
+
+	UPROPERTY()
+	TArray<FSavedActor> SavedActors;
+};
+
 USTRUCT(BlueprintType)
 struct FSavedAbility
 {
@@ -105,4 +138,10 @@ public:
 
 	UPROPERTY()
 	TArray<FSavedAbility> SavedAbilities;
+
+	UPROPERTY()
+	TArray<FSavedMap> SavedMaps;
+
+	FSavedMap GetSavedMapWithMapName(const FString& InMapName);
+	bool HasMap(const FString& InMapName);
 };
