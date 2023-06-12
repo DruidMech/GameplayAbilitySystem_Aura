d3cc6d005e6be4e89e9645ffcff18ea8c30dfde0
Stephen Ulibarri <druidmech@gmail.com>
2023-06-12 17:32:03 -0600

    Different Highlight Colors

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -5,6 +5,9 @@
 #include "CoreMinimal.h"
 
 #define CUSTOM_DEPTH_RED 250
+#define CUSTOM_DEPTH_BLUE 251
+#define CUSTOM_DEPTH_TAN 252
+
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
 #define ECC_Target ECollisionChannel::ECC_GameTraceChannel2
 #define ECC_ExcludePlayers ECollisionChannel::ECC_GameTraceChannel3
